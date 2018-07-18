eb8f64a1fb73b7e7c46f67512f468322a6801e07
Christoph Doerbeck <38790538+xtophd@users.noreply.github.com>
2018-07-18 16:29:45 -0500

    Update pre-install-script.sh

--- pre-install-script.sh
+++ pre-install-script.sh
@@ -7,7 +7,7 @@
 ## WORKSTATION
 yum install -y wget git net-tools bind-utils yum-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct
 
-## NODES
+## CLUSTER
 for i in node1 node2 node3 ; do
   ssh $i "yum install -y wget git net-tools bind-utils yum-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct"
 done
@@ -15,8 +15,18 @@ done
 ## WORKSTATION
 yum -y install atomic-openshift-utils
 
-## NODES
+## CLUSTER
 for i in node1 node2 node3 ; do
   ssh $i "yum install -y docker-1.13.1"
 done
 
+## CLUSTER
+for i in node1 node2 node3 ; do
+  scp docker-storage-setup $i:/etc/sysconfig
+  
+  vgremove --force docker-vg
+  pvremove --force /dev/vdb
+  wipefs -a /dev/vdb
+  
+  ssh $i "docker-storage-setup"
+done
