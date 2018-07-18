3e914125863ee69c28a00febede265b4283bc671
Christoph Doerbeck <38790538+xtophd@users.noreply.github.com>
2018-07-18 15:55:25 -0500

    Update pre-install-script.sh

--- pre-install-script.sh
+++ pre-install-script.sh
@@ -4,11 +4,16 @@
 ## Most of this will get moved into an Ansible Playbook
 ##
 
-## set up workstation
+## WORKSTATION
 yum install -y wget git net-tools bind-utils yum-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct
 
-## set up cluster nodes
+## NODES
 for i in node1 node2 node3 ; do
   ssh $i "yum install -y wget git net-tools bind-utils yum-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct"
 done
+
+## WORKSTATION
+yum install atomic-openshift-utils
+
+
 
