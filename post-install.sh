f78044ec0d8e52d8f84ecfb7c4347a2480580cdb
Christoph Doerbeck <38790538+xtophd@users.noreply.github.com>
2018-07-19 11:30:41 -0500

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -2,3 +2,7 @@
 
 ## MASTER: install config to enable htpasswd oauth
 scp master-config.yaml master:/etc/origin/master
+
+## MASTER: install the httpd-utils so we can manage accounts for OCP
+ssh master "yum install -y httpd-utils ; touch /etc/origin/openshift-passwd"
+
