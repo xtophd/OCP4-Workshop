40104e2c3858774a7397ee03823022e7f07b1a38
Christoph Doerbeck <38790538+xtophd@users.noreply.github.com>
2018-07-19 15:41:01 -0500

    Update 01-pre-install-script.sh

--- 01-pre-install-script.sh
+++ 01-pre-install-script.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-##
-## Most of this will get moved into an Ansible Playbook
-##
+## This script is intended to be run on the control host (ie: workstation)
 
-
-## WORKSTATION: install the ansible hosts inventory
+## Install the ansible hosts inventory
 cp -f ./configs/ansible-hosts /etc/ansible/hosts
-
-
 
 ## Now we can do things via ansible where possible
 
+ansible-playbook -f 3 ./playbooks/workstation-pre-install.yml
+ansible-playbook -f 3 ./playbooks/cluster-pre-install.yml
 
 ## WORKSTATION:  install minimum tool requirements
 #yum install -y wget git net-tools bind-utils yum-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct
