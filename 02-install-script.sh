cfd6c18511814b7180532c63bf0eb4f2e305fae2
Christoph Doerbeck <38790538+xtophd@users.noreply.github.com>
2018-07-20 18:16:29 -0500

    Update 02-install-script.sh

--- 02-install-script.sh
+++ 02-install-script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ## WORKSTATION: run the installer
-time ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
+ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
