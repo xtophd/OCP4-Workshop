64c815c68dbef0584ea8f7e0fc95debce0318c83
Christoph Doerbeck <38790538+xtophd@users.noreply.github.com>
2018-07-20 10:29:28 -0500

    Update 01-pre-install-script.sh

--- 01-pre-install-script.sh
+++ 01-pre-install-script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-## This script is intended to be run on the control host (ie: workstation)
+## This script is intended to be run:
+##     on the control host (ie: workstation)
+##     CWD =  ~root/OCP-Workshop
 
 ##
 ## Install the ansible hosts inventory
