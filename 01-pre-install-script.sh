885c45a694b696eebfde0e2e97580d2b323b709d
Christoph Doerbeck <38790538+xtophd@users.noreply.github.com>
2018-07-20 10:26:41 -0500

    Update 01-pre-install-script.sh

--- 01-pre-install-script.sh
+++ 01-pre-install-script.sh
@@ -3,10 +3,11 @@
 ## This script is intended to be run on the control host (ie: workstation)
 
 ## Install the ansible hosts inventory
+echo "Installing config file: /etc/ansible/hosts"
 cp -f ./configs/ansible-hosts /etc/ansible/hosts
 
 ## Now we can do things via ansible where possible
 
 ## ansible-playbook -f 3 ./playbooks/workstation-pre-install.yml
-
+echo "Calling ansible playbook: ./playbooks/cluster-pre-install.yml"
 ansible-playbook -f 4 ./playbooks/cluster-pre-install.yml
