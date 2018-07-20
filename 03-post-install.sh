c75c60b2b6b125ad632dc8d6d888f292970d1141
Christoph Doerbeck <38790538+xtophd@users.noreply.github.com>
2018-07-20 09:24:23 -0500

    Delete 03-post-install.sh

--- 03-post-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## MASTER: install config to enable htpasswd oauth
-scp ./configs/master-config.yaml master:/etc/origin/master
-
-## MASTER: install the httpd-utils so we can manage accounts for OCP
-ssh master "yum install -y httpd-utils ; touch /etc/origin/openshift-passwd"
-
