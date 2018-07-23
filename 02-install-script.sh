66221e077decf31d49910d7c9c1f5badf8c74021
Christoph Doerbeck <38790538+xtophd@users.noreply.github.com>
2018-07-23 09:17:20 -0500

    Update and rename 02-install-script.sh to 02-ocp-install-script.sh

--- 02-install-script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-## WORKSTATION: run the installer
-ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
---
+++ 02-ocp-install-script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+## WORKSTATION: run the installer
+ansible-playbook -f 5 /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
