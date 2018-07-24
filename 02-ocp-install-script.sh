5b8911bf502998d220165c78ed34736e6fca8394
Christoph Doerbeck <38790538+xtophd@users.noreply.github.com>
2018-07-24 09:31:15 -0500

    Update 02-ocp-install-script.sh

--- 02-ocp-install-script.sh
+++ 02-ocp-install-script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ## WORKSTATION: run the installer
-ansible-playbook -f 5 /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
+time ansible-playbook -f 5 /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
