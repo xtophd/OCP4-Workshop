7410d7302accb7ed7e3bae9b82109026b545b9ab
Christoph Doerbeck <38790538+xtophd@users.noreply.github.com>
2018-07-24 18:08:18 -0500

    Update 02-ocp-install-script.sh

--- 02-ocp-install-script.sh
+++ 02-ocp-install-script.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+
 ## WORKSTATION: run the installer
+
+echo -e "\n## Calling ansible playbook: ./playbooks/workstation-pre-install.yml\n"
 time ansible-playbook -f 5 /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
