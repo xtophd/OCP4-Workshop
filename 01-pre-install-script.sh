eef9e4d06ad54f3cf1a7848bfa98fc86de12f040
Christoph Doerbeck <38790538+xtophd@users.noreply.github.com>
2018-07-24 08:48:09 -0500

    Update 01-pre-install-script.sh
    minor changes to output
    wrapped ansible-playbook calls in 'time'

--- 01-pre-install-script.sh
+++ 01-pre-install-script.sh
@@ -7,15 +7,15 @@
 ##
 ## Install the ansible hosts inventory
 ##
-echo "Installing config file: /etc/ansible/hosts"
+echo "\n## Installing config file: /etc/ansible/hosts"
 cp -f ./configs/ansible-hosts /etc/ansible/hosts
 
 ##
 ## Now we can do things via ansible where possible
 ##
 
-echo "Calling ansible playbook: ./playbooks/workstation-pre-install.yml"
-ansible-playbook -f 4 ./playbooks/workstation-pre-install.yml
+echo "\n## Calling ansible playbook: ./playbooks/workstation-pre-install.yml"
+time ansible-playbook ./playbooks/workstation-pre-install.yml
 
-echo "Calling ansible playbook: ./playbooks/cluster-pre-install.yml"
-ansible-playbook -f 4 ./playbooks/cluster-pre-install.yml
+echo "\n## Calling ansible playbook: ./playbooks/cluster-pre-install.yml"
+time ansible-playbook -f 4 ./playbooks/cluster-pre-install.yml
