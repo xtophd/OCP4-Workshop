7cb3709ae8e5f0e1f566bd4dc070380690afba9a
Christoph Doerbeck <38790538+xtophd@users.noreply.github.com>
2018-07-24 18:06:19 -0500

    Update 01-pre-install-script.sh

--- 01-pre-install-script.sh
+++ 01-pre-install-script.sh
@@ -7,15 +7,15 @@
 ##
 ## Install the ansible hosts inventory
 ##
-echo "\n## Installing config file: /etc/ansible/hosts"
+echo -e '\n## Installing config file: /etc/ansible/hosts\n"
 cp -f ./configs/ansible-hosts /etc/ansible/hosts
 
 ##
 ## Now we can do things via ansible where possible
 ##
 
-echo "\n## Calling ansible playbook: ./playbooks/workstation-pre-install.yml"
+echo -e "\n## Calling ansible playbook: ./playbooks/workstation-pre-install.yml\n"
 time ansible-playbook ./playbooks/workstation-pre-install.yml
 
-echo "\n## Calling ansible playbook: ./playbooks/cluster-pre-install.yml"
+echo -e "\n## Calling ansible playbook: ./playbooks/cluster-pre-install.yml\n"
 time ansible-playbook -f 4 ./playbooks/cluster-pre-install.yml
