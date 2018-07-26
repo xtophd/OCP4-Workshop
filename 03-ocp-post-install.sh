aaa6179a5838a8481eafd17320aaa4edb2b66a91
Christoph Doerbeck <38790538+xtophd@users.noreply.github.com>
2018-07-26 14:00:56 -0400

    Create 03-ocp-post-install.sh

---
+++ 03-ocp-post-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+## WORKSTATION: run post installation steps
+
+echo ""
+echo "## Calling ansible playbook: ./playbooks/ocp-post-install.yml"
+echo ""
+time ansible-playbook -f 5 ./playbooks/ocp-post-install..yml
