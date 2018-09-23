518c349e400da77514b85ef20ecfd9bdf040e17d
Christoph Doerbeck <38790538+xtophd@users.noreply.github.com>
2018-09-23 05:39:50 -0500

    Update index.php

--- src/bluegreen/index.php
+++ src/bluegreen/index.php
@@ -1,17 +1,71 @@
 <?php
 
-$myColor = 'grey';
+//
+//  This program has 2 modes:
+//    myMode = worker myColor = {red,blue,grean}
+//      
+//      Output => myMode, myColor, HOSTNAME
+//
+//    myMode = watcher myRoute = {exposed route to watch}
+//
+//      Output => matrix of results calling OCP route
+//
+//  You set myMode, myColor, myRoute are ENV variables in the OCP deployment config
+//
 
-$envColor    = getenv("myCOLOR");
+// Initialize Variables
+$myColor  = 'unset';
+$myMode   = 'unset';
+$myRoute  = 'unset';
 
-if (( $envColor == 'blue') || ($envColor == 'green')) {
-  $myColor = $envColor;
+$envMode    = getenv("myMode");
+
+if ( $envMode == "watcher) {
+
+    // Set a refresh header
+    header("Refresh:3");
+
+    $envRoute   = getenv("myRoute");
+
+                 
+    // Collect route results until we get repetition, then exit
+
+    // until workerStatus[HOSTNAME] != ''
+    //   podStatus = curl myRoute
+    //   workStatus[podStatus[myHostname]] = podstatus[myColor]
+    //
+
+    $output = [ 'myMode'     => $myMode,
+                'myRoute'    => $myRoute, 
+                'myHostname' => getenv("HOSTNAME"),
+                'myStatus'   => "ok" ];
+
+} elseif ( $envMode == "worker" ) {
+
+    $envColor   = getenv("myColor");
+
+    if (( $envColor == 'blue') || ($envColor == 'green') || ($envColor == 'red' )) {
+        $myColor = $envColor;
+    }
+
+    $output = [ 'myMode'     => $myMode,
+                'myColor'    => $myColor, 
+                'myHostname' => getenv("HOSTNAME"),
+                'myStatus'   => "ok" ];
+                
+} else {
+
+    // This is essentially the error mode output
+
+    $output = [ 'myMode'     => $myMode,
+                'myColor'    => $myColor,
+                'myRoute'    => $myRoute,
+                'myStatus'   => "error" ];
 }
 
-$output = [ 'HOSTNAME' =>  getenv("HOSTNAME"),
-            'myCOLOR'  => $myColor ];
+// Send output
 
 echo json_encode( $output );
-
 echo "\n";
+
 ?>
