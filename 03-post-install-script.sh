93c5774fb14725a74b63c40c7ed51827f5884510
Christoph Doerbeck <38790538+xtophd@users.noreply.github.com>
2019-01-08 13:05:30 -0600

    Delete 03-post-install-script.sh
    merged into install-openshift.sh

--- 03-post-install-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-## WORKSTATION: run post installation steps
-
-echo ""
-echo "## Calling ansible playbook: ./playbooks/ocp-post-install.yml"
-echo ""
-time ansible-playbook -f 5 ./playbooks/cluster-post-install.yml
