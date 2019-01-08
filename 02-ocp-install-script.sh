dc910e5e370ca5b0b6c85427e29225dde122d88a
Christoph Doerbeck <38790538+xtophd@users.noreply.github.com>
2019-01-08 13:05:15 -0600

    Delete 02-ocp-install-script.sh
    merged into install-openshift.sh

--- 02-ocp-install-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-## WORKSTATION: run the installer
-
-echo ""
-echo "## Calling ansible playbook: ./playbooks/workstation-pre-install.yml"
-echo ""
-time ansible-playbook -f 5 /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
