4657bb2094ed991c193009f9ca34b4c28424a9fc
Christoph Doerbeck <38790538+xtophd@users.noreply.github.com>
2019-01-14 12:56:06 -0600

    Update install-openshift.sh

--- install-openshift.sh
+++ install-openshift.sh
@@ -11,30 +11,8 @@ if [ ! -e "${myInventory}" ] ; then
    exit
 fi
 
-echo ""
-echo "## Calling ansible playbook: ./playbooks/workstation-pre-install.yml"
-echo ""
-
-time ansible-playbook -i ${myInventory} ./playbooks/workstation-pre-install.yml
-
-echo ""
-echo "## Calling ansible playbook: ./playbooks/cluster-pre-install.yml"
-echo ""
-
-time ansible-playbook -i ${myInventory} -f 5 ./playbooks/cluster-pre-install.yml
-
-## Run the installer
-
-echo ""
-echo "## Calling ansible playbook: ./playbooks/workstation-pre-install.yml"
-echo ""
-
-time ansible-playbook -i ${myInventory} -f 5 /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
-
-## Run post installation steps
-
-echo ""
-echo "## Calling ansible playbook: ./playbooks/ocp-post-install.yml"
-echo ""
-
-time ansible-playbook -i ${myInventory} -f 5 ./playbooks/cluster-post-install.yml
+time ansible-playbook -i ${myInventory} -5 \
+   ./playbooks/workstation-pre-install.yml \
+   ./playbooks/cluster-pre-install.yml \
+   /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml \
+   ./playbooks/cluster-post-install.yml
