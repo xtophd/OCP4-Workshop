2254d4496e49a888f6edb285df8a66c673451a2d
Christoph Doerbeck <38790538+xtophd@users.noreply.github.com>
2019-02-25 12:39:10 -0600

    Create beta-install-openshift-3.11.sh

---
+++ beta-install-openshift-3.11.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+## This script is intended to be run:
+##     on the control host (ie: workstation)
+##     CWD =  ~root/OCP-Workshop
+
+myInventory="./configs/ocp-workshop"
+
+if [ ! -e "${myInventory}" ] ; then
+    echo "ERROR: Are you in the right directory? Can not find ${myInventory}" ; exit
+    exit
+fi
+
+time ansible-playbook -i ${myInventory} -f 5 ./playbooks/workstation-pre-install.yml
+   
+## If previous cmd exited non-zero then exit
+if [ "$?" -ne "0" ] ; then
+    echo "ERROR: previous command exited non-zero, check errors and try again"
+    exit
+fi
+   
+## *** WARNING *** WARNING *** WARNING *** WARNING *** WARNING ***
+##  We have to break up the workstation and cluster playbook runs because the 
+##  workstation playbook (above) installs ansible libraries.  So ansible needs 
+##  to exit to run properly the next time (below).  Don't consolidate!!!
+## *** WARNING *** WARNING *** WARNING *** WARNING *** WARNING ***
+
+time ansible-playbook -i ${myInventory} -f 5 \
+    ./playbooks/cluster-pre-install.yml \
+    /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml \
+    ./playbooks/cluster-post-install.yml
