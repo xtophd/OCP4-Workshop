0896c70f4f359cce682e79b3d40eacb63489d149
root <root@rhv2.lab.linuxsoup.com>
2020-05-11 01:50:53 +0000

    still trying to get rewrite committed

---
+++ prepare-kvm-host.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+##
+## NOTE: you must point to the correct inventory and extravars yml
+##
+##   Take a sample configs from ./sample-configs and 
+##   copy it to ./playbooks/config/{master,libvirt}-config.yml
+##
+
+myInventory="./config/master-config.yml"
+myExtravars="./config/libvirt-config.yml"
+
+## This script is intended to be run:
+##     on the libvirt hypervisor node
+##     in the project directory
+##     EX: CWD == ~root/OCP4-Workshop
+
+if [[ ! -e "${myInventory}" || ! -e "${myExtravars}" || ! -d "./playbooks" ]] ; then
+    echo "ERROR: Are you in the right directory? Can not find ${myInventory} | ${myExtravars} | ./playbooks " ; exit
+    exit
+fi
+
+##
+##
+##
+
+case "$1" in
+    "all")
+
+        echo "ansible-playbook -i ${myInventory} -e @${myExtravars} -f 10  ./playbooks.deployer-kvm/libvirt.yml"
+        time  ansible-playbook -i ${myInventory} -e @${myExtravars} -f 10  ./playbooks.deployer-kvm/libvirt.yml 
+        ;;
+
+    "deploy"     | \
+    "basics"     | \
+    "nested"     | \
+    "cockpit"    | \
+    "network"    | \
+    "dns"        | \
+    "haproxy"    | \
+    "bastion"    | \
+    "nodes"      | \
+    "postconfig" | \
+    "postinstall")
+
+        echo "ansible-playbook -i ${myInventory} -e @${myExtravars} -f 10  --tags $1 ./playbooks.deployer-kvm/libvirt.yml"
+        time  ansible-playbook -i ${myInventory} -e @${myExtravars} -f 10  --tags $1 ./playbooks.deployer-kvm/libvirt.yml 
+        ;;
+
+    *)
+        echo "USAGE: prepare-kvm-host.sh [ all | deploy | basics | nested | cockpit | network | dns | haproxy | bastion | nodes | postconfig | postinstall ]"
+
+        ;;
+
+esac         
---
+++ prepare-ocp4-workshop.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+##
+## NOTE: you must point to the correct inventory
+##
+##   Take a sample config from ./configs and
+##   copy it to ./playbooks/vars-custom/master-config.yml
+##
+
+myInventory="./config/master-config.yml"
+myExtravars="./config/libvirt-config.yml"
+
+## This script is intended to be run:
+##     on the libvirt hypervisor node
+##     in the project directory
+##     EX: CWD == ~root/OCP4-Workshop
+
+if [ ! -e "${myInventory}" ] ; then
+    echo "ERROR: Are you in the right directory? Can not find ${myInventory}" ; exit
+    exit
+fi
+
+if [ ! -e "./playbooks" ] ; then
+    echo "ERROR: Are you in the right directory? Can not find ./playbooks" ; exit
+    exit
+fi
+
+##
+##
+##
+
+case "$1" in
+    "all")
+        echo "ansible-playbook -i ${myInventory} -e @${myExtravars} -f 10  ./playbooks/bastion-setup.yml"
+        time  ansible-playbook -i ${myInventory} -e @${myExtravars} -f 10  ./playbooks/bastion-setup.yml
+        ;;
+         
+    "dns"       | \
+    "dhcp"      | \
+    "nfs"       | \
+    "ntp"       | \
+    "haproxy"   | \
+    "matchbox"  | \
+    "httpd"     | \
+    "openshift" | \
+    "finish")
+
+        echo "ansible-playbook -i ${myInventory} -e @${myExtravars} -f 10 --tags $1 ./playbooks/bastion-setup.yml"
+        time  ansible-playbook -i ${myInventory} -e @${myExtravars} -f 10 --tags $1 ./playbooks/bastion-setup.yml
+        ;;
+
+    *)
+        echo "USAGE: bastion-setup.sh [ all | dns | dhcp | ntp | haproxy | matchbox | httpd | openshift | finish ]"
+        ;;
+
+esac         
+
