946e5375d182d01500c13f23dba5c3e2dff6919d
root <root@node2.telcolab.example.com>
2021-02-23 08:19:25 -0500

    UPDATE: changed behaviour of rhel8-rhsm.sh to leave system registered.

---
+++ scripts/rhel8-rhsm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## This script is designed to quickly take care of registering
+## this client to rhsm for OS packages and updates and installing ansible
+
+subscription-manager register
+
+subscription-manager attach
+
+subscription-manager repos --disable=*
+
+subscription-manager repos --enable=ansible-2.9-for-rhel-8-x86_64-rpms \
+                           --enable=rhel-8-for-x86_64-appstream-rpms   \
+                           --enable=rhel-8-for-x86_64-baseos-rpms
+
+yum install -y ansible
+
