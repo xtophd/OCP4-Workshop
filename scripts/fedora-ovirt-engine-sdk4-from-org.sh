b25638e6e8db73b1d4a38d8781c1d07cf7bd33e7
Super User <root@fedora38.lab.linuxsoup.com>
2023-12-04 15:47:11 -0500

    UPDATE: deployer UDPATE: couple of scripts to aid setup on fedora (although ovirt SDK will not run on fedora)

---
+++ scripts/fedora-ovirt-engine-sdk4-from-org.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo ""
+echo "# WARNING: unfortunately there is no functional ovirt sdk for fedora38!!!"
+echo "#          the SDK has a dependency on python(api) == 3.6"
+echo ""
+
+
