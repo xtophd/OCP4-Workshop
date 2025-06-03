345ea87906b1d3f3ba4e6feec086e620102d31a9
xtophd <root@ainur-r8.lab.linuxsoup.com>
2025-06-03 10:04:18 -0400

    added ansible-ovirt-collection to needed packages for ovirt support in rhel8

--- scripts/rhel8-ovirt-engine-sdk4-from-rhsm.sh
+++ scripts/rhel8-ovirt-engine-sdk4-from-rhsm.sh
@@ -11,4 +11,4 @@ subscription-manager repos --enable=rhv-4.4-manager-for-rhel-8-x86_64-rpms \
                            --enable=rhel-8-for-x86_64-appstream-rpms   \
                            --enable=rhel-8-for-x86_64-baseos-rpms
 
-yum install -y python3-ovirt-engine-sdk4
+yum install -y python3-ovirt-engine-sdk4 ansible-ovirt-collection
